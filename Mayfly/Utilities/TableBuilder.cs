06122b2e80047316912bf5ce4085622331967e4e
Kerillian <49354481+Kerillian@users.noreply.github.com>
2022-04-09 13:21:02 -0400

    Improved TableBuilder.

--- Mayfly/Utilities/TableBuilder.cs
+++ Mayfly/Utilities/TableBuilder.cs
@@ -2,14 +2,46 @@
 
 namespace Mayfly.Utilities
 {
+	public class TableDesign
+	{
+		public char Horizontal { get; set; }
+		public char Vertical { get; set; }
+		public char TopLeft { get; set; }
+		public char TopConnector { get; set; }
+		public char TopRight { get; set; }
+		public char HeaderLeft { get; set; }
+		public char HeaderConnector { get; set; }
+		public char HeaderRight { get; set; }
+		public char BottomLeft { get; set; }
+		public char BottomConnector { get; set; }
+		public char BottomRight { get; set; }
+
+		public static TableDesign Default { get; } = new TableDesign()
+		{
+			Horizontal = '─',
+			Vertical = '│',
+			TopLeft = '╭',
+			TopConnector = '┬',
+			TopRight = '╮',
+			HeaderLeft = '├',
+			HeaderConnector = '┼',
+			HeaderRight = '┤',
+			BottomLeft = '╰',
+			BottomConnector = '┴',
+			BottomRight = '╯'
+		};
+	}
+
 	public class TableBuilder
 	{
 		private readonly string[] tableHeaders;
 		private readonly int[] sizes;
 		private readonly List<string[]> tableRows = new List<string[]>();
+		private readonly TableDesign design;
 
 		public TableBuilder(params string[] headers)
 		{
+			this.design = TableDesign.Default;
 			this.tableHeaders = headers;
 			this.sizes = new int[headers.Length];
 
@@ -17,6 +49,11 @@ namespace Mayfly.Utilities
 			{
 				this.sizes[i] = this.tableHeaders[i].Length;
 			}
+		}
+
+		public TableBuilder(TableDesign design, params string[] headers) : this(headers)
+		{
+			this.design = design;
 		}
 
 		public void AddRow(params string[] row)
@@ -43,23 +80,44 @@ namespace Mayfly.Utilities
 			{
 				parts[i] = $"{{{i},-{this.sizes[i]}}}";
 			}
+			
+			return $"{this.design.Vertical} {string.Join($" {this.design.Vertical} ", parts)} {this.design.Vertical}\n";
+		}
 
-			return "| " + string.Join(" | ", parts) + " |\n";
+		public string BuildFiller(char left, char connector, char right)
+		{
+			StringBuilder builder = new StringBuilder();
+
+			builder.Append(left);
+
+			for (int i = 0; i < this.sizes.Length; i++)
+			{
+				builder.Append(new string(design.Horizontal, this.sizes[i] + 2));
+
+				if (i != this.sizes.Length - 1)
+				{
+					builder.Append(connector);
+				}
+			}
+			
+			return builder.Append(right).ToString();
 		}
 
 		public string Build()
 		{
 			StringBuilder builder = new StringBuilder();
 			string template = BuildTemplate();
-			
+
+			builder.AppendLine(this.BuildFiller(design.TopLeft, design.TopConnector, design.TopRight));
 			builder.AppendFormat(template, this.tableHeaders);
-			builder.AppendFormat(template, this.sizes.Select(x => new string('-', x)).ToArray());
+			builder.AppendLine(this.BuildFiller(design.HeaderLeft, design.HeaderConnector, design.HeaderRight));
 
 			foreach (string[] row in this.tableRows)
 			{
 				builder.AppendFormat(template, row);
 			}
 
+			builder.AppendLine(this.BuildFiller(design.BottomLeft, design.BottomConnector, design.BottomRight));
 			return builder.ToString();
 		}
 	}
