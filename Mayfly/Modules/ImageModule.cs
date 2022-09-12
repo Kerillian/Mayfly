e60eda53e99458b9bc2aef8976ca8226bed46669
Kerillian <49354481+Kerillian@users.noreply.github.com>
2022-09-12 16:54:37 -0400

    Cleanup usings.

--- Mayfly/Modules/ImageModule.cs
+++ Mayfly/Modules/ImageModule.cs
@@ -1,7 +1,4 @@
-using System;
-using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using Discord;
 using Discord.Interactions;
 using Mayfly.Attributes;
@@ -14,7 +11,6 @@ using Mayfly.Structures;
 using Mayfly.UrlBuilders;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using Image = SixLabors.ImageSharp.Image;
-using RuntimeResult = Discord.Interactions.RuntimeResult;
 
 namespace Mayfly.Modules
 {
