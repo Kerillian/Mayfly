bb3f74b53f2704d6a6b997c8e1afebb4b3884dbf
Kerillian <49354481+Kerillian@users.noreply.github.com>
2022-09-17 22:20:02 -0400

    Move Math command to UtilityModule and delete MathModule

--- Mayfly/Modules/UtilityModule.cs
+++ Mayfly/Modules/UtilityModule.cs
@@ -22,7 +22,7 @@ namespace Mayfly.Modules
 
 		public bool TryParseVideoID(string url, out string id)
 		{
-			id = default(string);
+			id = default;
 
 			if (string.IsNullOrWhiteSpace(url))
 				return false;
@@ -174,6 +174,28 @@ namespace Mayfly.Modules
 		public async Task Bobux(double bobux)
 		{
 			await RespondAsync($"That would be about `${bobux * 0.0125:0.##}`");
+		}
+		
+		[SlashCommand("math", "Calculate stuff.")]
+		public async Task<RuntimeResult> Calc(string expression)
+		{
+			await DeferAsync();
+			
+			if (expression.Replace(" ", "") == "9+10")
+			{
+				await RespondAsync("21");
+				return MayflyResult.FromSuccess();
+			}
+		
+			string calculated = await Http.GetStringAsync("https://api.mathjs.org/v4/?expr=" + Uri.EscapeDataString(expression));
+
+			if (calculated.Contains("Error"))
+			{
+				return MayflyResult.FromError("InvalidExpression", calculated.Replace("", "Error: "));
+			}
+
+			await FollowupAsync(calculated);
+			return MayflyResult.FromSuccess();
 		}
 	}
 }
